047805399a4138c83edcf6468bb01813789e5b01
William Bojczuk <williambojczuk@gmail.com>
2024-02-20 17:02:45 +0200

    init

---
+++ globals.d.ts
@@ -0,0 +1,19 @@
+interface faqType{
+    question: string,
+    answer: string
+}
+
+interface linkProps{
+    href: string,
+    target: string,
+    className: string,
+    style: any,
+    isLocal: boolean
+}
+
+interface reviewType{
+    title: string,
+    desc: string,
+    name: string,
+    role: string
+}
---
+++ src/app/(mainsite)/components/homepage/TestForm/TestForm.tsx
@@ -0,0 +1,11 @@
+import HorizontalFreeQuoteForm from "../../misc/HorizontalFreeQuoteForm/HorizontalFreeQuoteForm"
+
+export default function TestForm() {
+    
+
+  return (
+    <div className="center" style={{width: "100vw", height: "100vh"}}>
+      <HorizontalFreeQuoteForm />
+    </div>
+  )
+}
