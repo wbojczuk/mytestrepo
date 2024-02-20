4acb7c96b1b73f3fb7267074c1e2303bea260041
William Bojczuk <williambojczuk@gmail.com>
2024-02-20 17:29:05 +0200

    reset

--- src/app/controllers/getBlogs.ts
+++
@@ -1,7 +0,0 @@
-import { getDocuments } from "outstatic/server"
-
-export default function getBlogs(){
-    const posts = getDocuments('posts', ['title', "description", "author", "publishedAt", "slug", "coverImage"])
-
-  return posts
-}
